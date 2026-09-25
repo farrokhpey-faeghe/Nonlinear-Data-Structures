a762430e3b716359d03ad40dd982a5365c7f804a
farrokhpey-faeghe <164793806+farrokhpey-faeghe@users.noreply.github.com>
2026-09-24 20:25:26 -0400

    Create main.cpp

---
+++ HW2/Lab-Activity/main.cpp
@@ -0,0 +1,12 @@
+#include "GradeHistogram.h"
+
+int main()
+{
+    GradeHistogram gradeProgram;
+
+    gradeProgram.countGradesAsEntered();
+
+    gradeProgram.storeGradesThenCount();
+
+    return 0;
+}
